964c4dde8079673c5e15764982023f00c0a097df
cmoro2 <c.moro@mail.utoronto.ca>
2017-02-08 14:24:54 -0500

    Display Transcript text to terminal

--- converse/stt.py
+++ converse/stt.py
@@ -1,3 +1,4 @@
+import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from ws4py.client.threadedclient import WebSocketClient
@@ -8,17 +9,15 @@ def getEnv():
 	load_dotenv(dotenv_path)
 	username = os.environ.get("SPEECH_TO_TEXT_USERNAME")
 	password = os.environ.get("SPEECH_TO_TEXT_PASSWORD")
-        auth_string = "%s:%s" % (username, password)
+	auth_string = "%s:%s" % (username, password)
 	return auth_string	
 
 class SpeechToTextClient(WebSocketClient):
     def __init__(self):
         ws_url = "wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
-	environment = getEnv()
-        base64string = base64.encodestring(auth_string).replace("\n", "")
-
+        environment = getEnv()
+        base64string = base64.encodestring(environment).replace("\n", "")
         self.listening = False
-
         try:
             WebSocketClient.__init__(self, ws_url,
                 headers=[("Authorization", "Basic %s" % base64string)])
@@ -26,7 +25,7 @@ class SpeechToTextClient(WebSocketClient):
         except: print "Failed to open WebSocket."
 
     def opened(self):
-        self.send('{"action": "start", "content-type": "audio/l16;rate=16000"}')
+        self.send('{"action": "start", "content-type": "audio/l16;rate=16000", "inactivity_timeout": 5}')
         self.stream_audio_thread = threading.Thread(target=self.stream_audio)
         self.stream_audio_thread.start()
 
@@ -37,9 +36,21 @@ class SpeechToTextClient(WebSocketClient):
                 self.listening = True
         print "Message received: " + str(message)
 
+        if "results" in message:
+        	content = ""
+
+        	if (len(message["results"])==0):
+        		print("Watson returned an empty STT guess")
+        	else:
+        		content = message["results"][0]["alternatives"][0]["transcript"]
+        		print content
+        		#SpeechToTextClient.close(self)
+        		self.listening = False
+
+
     def stream_audio(self):
         while not self.listening:
-            time.sleep(0.1)
+        	time.sleep(0.1)
 
         reccmd = ["arecord", "-f", "S16_LE", "-r", "16000", "-t", "raw"]
         p = subprocess.Popen(reccmd, stdout=subprocess.PIPE)
@@ -47,15 +58,24 @@ class SpeechToTextClient(WebSocketClient):
         while self.listening:
             data = p.stdout.read(1024)
 
-            try: self.send(bytearray(data), binary=True)
-            except ssl.SSLError: pass
+            try: 
+            	self.send(bytearray(data), binary=True)
+            	#print ("testing testing")
+            except ssl.SSLError: 
+            	print("SSLError")
+            	pass
 
+        print("done looping")
         p.kill()
+        print "killed it"
 
     def close(self):
         self.listening = False
+        print("Getting ready to exit thread...")
         self.stream_audio_thread.join()
+        print("Exited thread successfuly")
         WebSocketClient.close(self)
+
 
 try:
     stt_client = SpeechToTextClient()
